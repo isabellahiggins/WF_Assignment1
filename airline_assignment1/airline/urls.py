982bb1b2b075ef0691ec4a2880e3c5046f817725
isabellahiggins <b00162371@mytudublin.ie>
2026-04-25 01:19:14 +0100

    added flight details and index template

--- airline_assignment1/airline/urls.py
+++ airline_assignment1/airline/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('flight/<int:flight_id>/', views.flight_detail, name='flight_detail'),
+    path('book/<int:flight_id>/', views.book_a_flight, name='book_a_flight'),
+    path('bookings/', views.bookings, name='bookings'),
+    path('invoices/', views.invoices, name='invoices'),
+
+]
--- airline_assignment1/airline/views.py
+++ airline_assignment1/airline/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Flight, Booking, Invoice
 
 # Create your views here.
+def index(request):
+    flights = Flight.objects.all()
+    return render(request, 'airline/index.html', {'flights': flights})
+
+def flight_detail(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, 'airline/flight_detail.html', {'flight': flight})
+
+def book_a_flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, 'airline/book_a_flight.html', {'flight': flight})
+
+def bookings(request):
+    bookings = Booking.objects.all()
+    return render(request, 'airline/bookings.html', {'bookings': bookings})
+
+def invoices(request):
+    invoices = Invoice.objects.all()
+    return render(request, 'airline/invoices.html', {'invoices': invoices})
